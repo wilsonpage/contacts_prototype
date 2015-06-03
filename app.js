4dc8aea130241aafbb51cdb75f07226b181c0c22
Wilson Page <wilsonpage@me.com>
2015-06-03 19:32:10 +0100

    Make em race

--- app.js
+++ app.js
@@ -1,26 +1,40 @@
-performance.mark('app-script');
-performance.measure('domLoading -> app-script', 'domLoading', 'app-script');
-
+var perf = window.performance;
 var channel = new BroadcastChannel('mychannel');
 
 channel.onmessage = e => {
   switch (e.data) {
     case 'connect':
-      performance.mark('got-connect');
-      performance.measure('domLoading -> got-connect', 'domLoading', 'got-connect');
-      var iframe = document.createElement('iframe');
-      iframe.src = 'services/contacts.html';
-      iframe.hidden = true;
-      document.body.appendChild(iframe);
+      performance.mark('message (BC)');
+      performance.measure('domLoading -> message (BC)', 'domLoading', 'message (BC)');
+      done();
     break;
   }
 };
 
+addEventListener('message', e => {
+  switch (e.data) {
+    case 'connect':
+      performance.mark('message (PM)');
+      performance.measure('domLoading -> message (PM)', 'domLoading', 'message (PM)');
+    break;
+  }
+});
+
+function done() {
+  perf.getEntriesByType('measure').forEach(entry => {
+    var name = entry.name;
+    var previous = Number(localStorage[name + ':total'] || 0);
+    var count = Number(localStorage[name + ':count'] || 0) + 1;
+    var total = previous += entry.duration;
+    var mean = total / count;
+
+    localStorage[name + ':total'] = total;
+    localStorage[name + ':count'] = count;
+
+    console.log('"' + name + '": ', mean + 'ms (mean)');
+  });
+}
+
 function clearStorage() {
   localStorage.clear();
 }
-
-onload = function() {
-  performance.mark('app-onload');
-  performance.measure('domLoading -> app-onload', 'domLoading', 'app-onload');
-};
